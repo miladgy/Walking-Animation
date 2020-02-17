e2a3f045d9a4681864bf6417dd840c7b64889f0e
Milad Ghadami Yazdi <milad.ghadami@appliedtechnology.se>
2020-02-17 16:34:33 +0100

    Add movement and positioning logic to the character

--- walk-animation.js
+++ walk-animation.js
@@ -2,17 +2,66 @@ window.onload = () => {
   const canvas = document.getElementById("walking-animation");
   const context = canvas.getContext("2d");
 
+  moveX = 80;
+  moveY = 250;
+  moveInXDirection = 10;
+  isBackgroundLoaded = false;
+  isCharacterLoaded = false;
+
   const backgroundImage = new Image();
-  backgroundImage.src = "background.png"
+  backgroundImage.src = "background.png";
   backgroundImage.onload = () => {
-    context.drawImage(backgroundImage, 0, 0, canvas.clientWidth, canvas.height)
+    isBackgroundLoaded = true;
   };
-  const spriteImage = new Image()
-  spriteImage.src="sprite.png"
+  const spriteImage = new Image();
+  spriteImage.src = "sprite.png";
 
   spriteImage.onload = () => {
-    context.drawImage(spriteImage, 0, 0, 256, 256, 80 , 260, 100, 100)
-  }
+    isCharacterLoaded = true;
+  };
+  // animation speed & timing
+  let animationStart = new Date();
 
+  // cell specs
+  cellWidth = 256;
+  cellHeight = 256;
+  currentCell = 0;
 
+  const animationLoop = () => {
+    let animationNow = new Date();
+
+    if (animationNow - animationStart >= 100) {
+      animationStart = animationNow;
+      // clear canvas
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // update
+      currentCell++;
+      currentCell %= 6;
+      if (moveX > canvas.width) {
+        moveX = Math.ceil(-1 * (cellWidth / 2));
+      } else {
+        moveX += moveInXDirection;
+      }
+      // draw
+      if (isBackgroundLoaded) {
+        context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+      }
+      if (isCharacterLoaded) {
+        context.drawImage(
+          spriteImage,
+          currentCell * cellWidth,
+          0,
+          cellWidth,
+          cellHeight,
+          moveX,
+          moveY,
+          100,
+          100
+        );
+      }
+    }
+    window.requestAnimationFrame(animationLoop);
+  };
+  window.requestAnimationFrame(animationLoop);
 };
