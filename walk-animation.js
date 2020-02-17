a3b0c625759e9c01175ad84c570cfdbf9d4b10fa
Milad Ghadami Yazdi <milad.ghadami@appliedtechnology.se>
2020-02-17 14:45:34 +0100

    Add background and character to the canvas

--- walk-animation.js
+++ walk-animation.js
@@ -1,6 +1,18 @@
 window.onload = () => {
-    const canvas = document.getElementById("walking-animation")
-    const context = canvas.getContext("2d")
+  const canvas = document.getElementById("walking-animation");
+  const context = canvas.getContext("2d");
+
+  const backgroundImage = new Image();
+  backgroundImage.src = "background.png"
+  backgroundImage.onload = () => {
+    context.drawImage(backgroundImage, 0, 0, canvas.clientWidth, canvas.height)
+  };
+  const spriteImage = new Image()
+  spriteImage.src="sprite.png"
+
+  spriteImage.onload = () => {
+    context.drawImage(spriteImage, 0, 0, 256, 256, 80 , 260, 100, 100)
+  }
 
 
-}
+};
